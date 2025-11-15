21944ecd0e4ab7e5ee470d82e35f25e5c0ccc5c5
Catchet <adam.agren@gmail.com>
2025-11-16 02:05:14 +0900

    Added serde attribute to keep Rust and JSON casings different

--- src/lib.rs
+++ src/lib.rs
@@ -23,10 +23,11 @@ pub struct ChessPosStats {
 }
 
 #[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct ChessMoveStats {
     pub uci: String,
     pub san: String,
-    pub averageRating: i32,
+    pub average_rating: i32,
     pub white: i32,
     pub draws: i32,
     pub black: i32,
@@ -41,11 +42,12 @@ pub struct ChessOpening {
 }
 
 #[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct LichessQueryParams {
     fen: String,
     speeds: Vec<String>,
-    topGames: u8,
-    recentGames: u8,
+    top_games: u8,
+    recent_games: u8,
 }
 
 const LICHESS_API_URL: &str = "https://api.lichess.ovh/opening-explorer";
@@ -53,8 +55,8 @@ pub async fn query_lichess(client: &reqwest::Client, fen: Fen) -> Result<ChessPo
     let params = LichessQueryParams {
         fen: fen.to_string(),
         speeds: vec![String::from("blitz"), String::from("rapid"), String::from("classical"), String::from("correspondence")],
-        topGames: 0,
-        recentGames: 0
+        top_games: 0,
+        recent_games: 0
     };
     let response = client.get(LICHESS_API_URL).query(&params).send().await?;
     let data: ChessPosStats = response.json().await?;
