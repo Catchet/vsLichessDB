05cd55830cbaca0c35300ce9c18f9104d6fa826a
Catchet <adam.agren@gmail.com>
2025-11-18 19:49:36 +0900

    Refactored contents from lib.rs into separate files

---
+++ src/modules/chess/query_lichess.rs
@@ -0,0 +1,24 @@
+use shakmaty::fen::Fen;
+
+use crate::{
+    errors::ApiError,
+    modules::chess::models::{ChessPosStats, LichessQueryParams},
+};
+
+const LICHESS_API_URL: &str = "https://api.lichess.ovh/opening-explorer";
+pub async fn query_lichess(client: &reqwest::Client, fen: Fen) -> Result<ChessPosStats, ApiError> {
+    let params = LichessQueryParams {
+        fen: fen.to_string(),
+        speeds: vec![
+            String::from("blitz"),
+            String::from("rapid"),
+            String::from("classical"),
+            String::from("correspondence"),
+        ],
+        top_games: 0,
+        recent_games: 0,
+    };
+    let response = client.get(LICHESS_API_URL).query(&params).send().await?;
+    let data: ChessPosStats = response.json().await?;
+    Ok(data)
+}
