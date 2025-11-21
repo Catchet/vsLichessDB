311f5f8420879e0a5f90fe0e8c1d4afceb8249d7
Catchet <adam.agren@gmail.com>
2025-11-21 20:34:30 +0900

    Extended random opponent to support playing as either white or black side

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,20 +6,25 @@ import "../assets/chessground.css";
 import "../assets/board.css";
 import "../assets/style.css";
 
+let playSide: "white" | "black" = "white";
+
 // init();
 init("rnbqkbnr/ppppppp1/8/7p/3PP3/8/PPP2PPP/RNBQKBNR b KQkq - 0 2");
 export function init(fen?: string) {
-  let board = vsRandom(document.getElementById("cg-wrap")!, fen);
+  let board = vsRandom(document.getElementById("cg-wrap")!, fen, playSide);
 }
 
-export function vsRandom(boardElem: HTMLElement, fen?: string) {
+export function vsRandom(boardElem: HTMLElement, fen?: string, playSide?: "white" | "black") {
+  const delay = 300;
+  const firstMove = false;
   const chess = new Chess(fen);
   const cg = Chessground(boardElem, {
     fen: fen,
     turnColor: toColour(chess),
+    orientation: playSide,
     check: chess.isCheck(),
     movable: {
-      color: toColour(chess),
+      color: playSide,
       free: false,
       dests: toDests(chess),
     },
@@ -27,14 +32,15 @@ export function vsRandom(boardElem: HTMLElement, fen?: string) {
       check: true,
     },
   });
-  if (toColour(chess) == "black") cg.toggleOrientation();
   cg.set({
     movable: {
       events: {
-        after: randomPlay(cg, chess, 300, false),
+        after: randomPlay(cg, chess, delay, firstMove),
       },
     },
   });
+  if (playSide !== toColour(chess))
+    setTimeout(() => makeRandomMove(cg, chess, firstMove), delay);
   return cg;
 }
 
@@ -51,22 +57,24 @@ export function randomPlay(
         check: true,
       });
     }
-    setTimeout(() => {
-      const moves = chess.moves({ verbose: true });
-      const move = firstMove
-        ? moves[0]
-        : moves[Math.floor(Math.random() * moves.length)];
-      chess.move(move.san);
-      cg.move(move.from, move.to);
-      cg.set({
-        turnColor: toColour(chess),
-        check: chess.isCheck(),
-        movable: {
-          color: toColour(chess),
-          dests: toDests(chess),
-        },
-      });
-      cg.playPremove();
-    }, delay);
+    setTimeout(() => makeRandomMove(cg, chess, firstMove), delay);
   };
+}
+
+export function makeRandomMove(cg: Api, chess: Chess, firstMove: boolean) {
+  const moves = chess.moves({ verbose: true });
+  const move = firstMove
+    ? moves[0]
+    : moves[Math.floor(Math.random() * moves.length)];
+  chess.move(move.san);
+  cg.move(move.from, move.to);
+  cg.set({
+    turnColor: toColour(chess),
+    check: chess.isCheck(),
+    movable: {
+      color: toColour(chess),
+      dests: toDests(chess),
+    },
+  });
+  cg.playPremove();
 }
