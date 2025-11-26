9c8dc01ff715abb896f64cd6f9121c167c39d9b0
Catchet <adam.agren@gmail.com>
2025-11-26 09:47:24 +0900

    Added sled db to app state and removed rocksdb

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,20 @@
 use actix_web::{App, HttpServer, middleware::Logger, web};
-use rocksdb::{DB, Options};
 use std::sync::Arc;
 use vs_lichess_db::modules::{cache::models::ChessCache, chess};
 
 #[actix_web::main]
 pub async fn main() -> std::io::Result<()> {
-    let db_path = "./lichess_cache";
-    let mut opts = Options::default();
-    opts.create_if_missing(true);
+    let db = sled::open(db_path).expect("Failed to open sled database");
 
-    let db = Arc::new(DB::open(&opts, db_path).expect("Failed to establish database"));
+    let client = reqwest::Client::new();
 
     env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
 
     HttpServer::new(move || {
         App::new()
             .wrap(Logger::default())
-            .app_data(web::Data::new(reqwest::Client::new()))
-            .app_data(web::Data::new(ChessCache { db: db.clone() }))
+            .app_data(web::Data::new(client))
+            .app_data(web::Data::new(db))
             .service(chess::router::controller())
     })
     .bind(("127.0.0.1", 8080))?
